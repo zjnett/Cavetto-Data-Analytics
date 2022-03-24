33ddb11c1bda3f91e38dd1c61ce98c04c63c81f0
Zach Nett <zjnett@mac.com>
2022-03-24 14:20:32 -0700

    Add basic database and dashboard functionality

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import Dash, html, dcc
+import plotly.express as px
 import pandas as pd
+import db
 
+app = Dash(__name__)
+
+df, cx = db.database()
+print(cx)
+cx.close()
+
+fig = px.scatter(df, x="time", y="value")
+
+app.layout = html.Div(children=[
+    html.H1(children='Cavetto Data Visualization'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == "__main__":
+    df, cx = db.database()
+    app.run_server(debug=True)
+    cx.close()
--- db.py
+++ db.py
@@ -1,4 +1,8 @@
 import mysql.connector
+from mysql.connector import errorcode
+import pandas as pd
+import datetime 
+from random import randrange
 
 # Open file and read credentials into config dictionary
 def read_config(file):
@@ -9,14 +13,45 @@ def read_config(file):
             config[key] = val
     return config
 
-    # Create a database
-
 # Connect to the database
 def connect():
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="",
-        database="python_db"
-    )
-    return conn
+    config = read_config("config/database.ini") # hardcoded for now
+    try:
+        cnx = mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+      elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+      else:
+        print(err)
+    else:
+        return cnx
+    return None
+
+# Generate random time
+def random_date(start):
+    start = start + datetime.timedelta(minutes=randrange(60))
+    return start
+
+# Main for testing
+def database():
+    cnx = connect()
+    if cnx:
+        # Query database
+        cursor = cnx.cursor()
+        query = ("SELECT * FROM `test_data`")
+        cursor.execute(query)
+        # Sample startdate
+        start = datetime.datetime(2022, 3, 22,13,00)
+        # Create empty pandas dataframe
+        df = pd.DataFrame(columns=['value', 'time'])
+        num_vals = cursor.rowcount
+        i = 0
+        for (value) in cursor:
+            # Add value to pandas dataframe
+            df_new_row = pd.DataFrame({'value' : value[0], 'time' : random_date(start)}, index=[i])
+            df = pd.concat([df, df_new_row])
+            i += 1
+        return df, cnx
+    return None, None
